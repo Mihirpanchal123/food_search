b84727c0d8ad8b9a2c4b88fd2d948256ba52f88c
Mihir panchal <mihirpanchal555@gmail.com>
2023-10-02 19:48:28 +0530

    env

--- project-4/app/src/components/SearchResults/SearchResult.jsx
+++ project-4/app/src/components/SearchResults/SearchResult.jsx
@@ -1,26 +1,26 @@
 import styled from "styled-components";
-import { BASE_URL, Button, Container } from "../../App";
+import { Button, Container } from "../../App";
 const SearchResult = ({ data }) => {
   return (
     <FoodCardContainer>
-    
+
       <FoodCards>
-          {data?.map(({ name, image, text, price }) => (
-            <FoodCard key={name}>
-              <div className="food_image">
-                <img src={BASE_URL + image} />
+        {data?.map(({ name, image, text, price }) => (
+          <FoodCard key={name}>
+            <div className="food_image">
+              <img src={process.env.BASE_URL + image} />
+            </div>
+            <div className="food_info">
+              <div className="info">
+                <h3>{name}</h3>
+                <p>{text}</p>
               </div>
-              <div className="food_info">
-                <div className="info">
-                  <h3>{name}</h3>
-                  <p>{text}</p>
-                </div>
-                <Button>${price.toFixed(2)}</Button>
-              </div>
-            </FoodCard>
-          ))}
-        </FoodCards>
-        
+              <Button>${price.toFixed(2)}</Button>
+            </div>
+          </FoodCard>
+        ))}
+      </FoodCards>
+
     </FoodCardContainer>
   );
 };
